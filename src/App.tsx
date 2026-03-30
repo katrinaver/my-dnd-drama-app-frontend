e9e2d603d3cd44385ae9c197f7731172b0d81df9
Ekaterina <averkatrin@gmail.com>
2026-03-30 21:19:17 +0200

    add main page and create

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p  className="p-10 text-center bg-white flex justrify-center text-black">
+          New App
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
